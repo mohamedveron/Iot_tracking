7a90b25db666303e0577dc4aed2fe2d18132c460
mohamedveron <mohamedveron23@gmail.com>
2021-05-05 17:16:05 +0200

    add project layers

---
+++ tracking/src/main/java/com/vodafone/iot/tracking/controller/DeviceController.java
@@ -0,0 +1,7 @@
+package com.vodafone.iot.tracking.controller;
+
+import org.openapitools.api.DevicesApi;
+
+public class DeviceController implements DevicesApi{
+
+}
---
+++ tracking/src/main/java/com/vodafone/iot/tracking/service/DeviceService.java
@@ -0,0 +1,10 @@
+package com.vodafone.iot.tracking.service;
+
+import com.vodafone.iot.tracking.model.Device;
+import com.vodafone.iot.tracking.model.SIMCard;
+
+public interface DeviceService {
+	
+	public void createDevice(Device d);
+	public void addSIMCardToDevice(SIMCard card);
+}
